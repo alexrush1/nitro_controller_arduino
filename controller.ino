83276b378bd3e8720ab27935687cace6ee9850e7
Ivan Timofeev <43262229+alexrush1@users.noreply.github.com>
2020-05-08 19:51:02 +0700

    Update controller.ino

--- controller.ino
+++ controller.ino
@@ -1,14 +1,15 @@
 #define ON 1                                      //turned on
 #define OFF 0                                     //turned off
 #define VELOCITY_GAUGE_PIN 3                      //tachometer pin
-#define RELE_PIN  8                  //rele pin
-#define HIGH_RPM_MODE 7               //pin for 4500 min rpm
-#define CALCULATION_METHOD 6            //pin for 1/3 or 1/6 coefficient 
+#define RELE_PIN  12                  //rele pin
+#define HIGH_RPM_MODE 2               //pin for 4500 min rpm
+#define CALCULATION_METHOD1 5         //pin for 1/3 or 
+#define CALCULATION_METHOD2 8         //1/4 coefficient
 short cc = 0;
 double gz = 0;
 int minGZ = 67;
 int ready = 0;
-double coeff = 3;
+double coeff = 0;
 unsigned long old_time = 0;
 unsigned long new_time = 0;
 unsigned long counter = 0;
@@ -20,25 +21,27 @@ void setup() {
   
   digitalWrite(RELE_PIN, OFF);                    //rele turned off when arduino starts
 
-  pinMode(10, INPUT_PULLUP);
-  digitalWrite(10, HIGH);
+  pinMode(CALCULATION_METHOD1, INPUT_PULLUP);
+  pinMode(CALCULATION_METHOD2, INPUT_PULLUP);
+  pinMode(HIGH_RPM_MODE, INPUT_PULLUP);
   
-  //pinMode(CALCULATION_METHOD, INPUT_PULLUP);
   
   //debug only
-  pinMode(13, OUTPUT);
-  pinMode(4, OUTPUT);
-  tone(4, 30);
-  //old_time = -3500;
+  //pinMode(4, OUTPUT);
+  //tone(3, 100);
+  Serial.begin(115200);
 }
 
 void loop() {
-  Serial.begin(115200);
-  
-  if (digitalRead(CALCULATION_METHOD)) {
-    coeff = 6;
-  } else {
+  //gz = 100;
+  digitalWrite(13, ON);
+  if (!digitalRead(CALCULATION_METHOD1)) {
     coeff = 3;
+  } else if (!digitalRead(CALCULATION_METHOD2)) {
+    coeff = 4;  
+  } 
+  if ((digitalRead(CALCULATION_METHOD1) && digitalRead(CALCULATION_METHOD2)) == true) {
+    coeff = 1;
   }
   
   if (!digitalRead(HIGH_RPM_MODE)) {
@@ -46,19 +49,18 @@ void loop() {
   } else {
     minGZ = 67;
   }
-  Serial.println(digitalRead(10));
-  if (ready) {
-    if ((gz >= minGZ)&&(gz <= 101)) {
-      digitalWrite(13, ON);
+  if (coeff != 0) {
+    if (ready) {
+      if ((gz >= minGZ)&&(gz <= 101)) {
         digitalWrite(RELE_PIN, ON);
-      //Serial.println("HIGH PERFORMANCE!");
-    } else if ((gz < minGZ-5)||(gz > 101.5)) {
-      digitalWrite(13, OFF);
+        //Serial.println("HIGH PERFORMANCE!");
+      } else if ((gz < minGZ-5)||(gz > 102.5)) {
         digitalWrite(RELE_PIN, OFF);
-      //Serial.println("EMERGENCY SHUTDOWN!");
+        //Serial.println("EMERGENCY SHUTDOWN!");
+      }
     }
   }
-  delay(500);
+  delay(400);
 }
 
 void Gz() {
@@ -72,8 +74,9 @@ void Gz() {
     new_time = millis();
     gz = ((15.0 * 1000.0)/ (double)((new_time - old_time)* coeff));
     old_time = new_time;
-    //Serial.println(gz);
+    Serial.println(gz);
     counter = 0;
     ready = 1;
+    digitalWrite(13, OFF);
   }
 }
